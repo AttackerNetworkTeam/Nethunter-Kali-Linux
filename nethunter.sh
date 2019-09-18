7338e60a4175e991267f117ec84b5afac01a4248
AttackerNetworkTeam <55377078+AttackerNetworkTeam@users.noreply.github.com>
2019-09-19 04:09:23 +0700

    Add files via upload

---
+++ nethunter.sh
@@ -0,0 +1,36 @@
+#Acep X-Code
+#Attacker Nwtwork Team
+
+
+
+figlet -f slant "Nethunter" | lolcat
+echo "Coder By : Acep X-Code"
+echo "My Team : Attacker Network Team" |
+
+sleep 1
+
+
+echo "Menginstal Bahan"
+
+termux-setup-storage
+
+pkg install git -y
+
+cd /sdcard
+
+git clone https://github.com/aceptriana/Nethunter-Aplikasi
+
+sleep 2
+
+clear
+
+echo "Install Selesai"
+echo "Silahkan Ke FileManager"
+echo "Ke Penyimpanan Internal"
+echo "Buka Folder Nethunter-Aplikasi"
+
+
+sleep 5
+
+
+echo "Good Bye"
